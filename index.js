71a3034af375de95acc09d32e7f57fc35e24716e
Muhitori <turevichkolya@gmail.com>
2023-03-01 16:42:18 +0200

    reverse serverless functions

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-const express = require("express");
-const app = express();
-
-const path = require("path");
-const port = process.env.PORT || 3000;
-const webhookPath = process.env.WEBHOOK_PATH;
-
-app.use(express.static("static"));
-app.use(express.json());
-
 require("dotenv").config();
-
+const express = require("express");
+const path = require("path");
 const { Telegraf } = require("telegraf");
 
+const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const port = process.env.PORT || 3000;
+const route = "/secret-path";
+
 bot.command("start", (ctx) => {
-	bot.telegram.sendMessage(ctx.chat.id, "Hello there!");
+	const inlineKeyboard = {
+		inline_keyboard: [
+			[
+				{
+					text: "Button 1",
+					callback_data: "button1",
+				},
+				{
+					text: "Button 2",
+					callback_data: "button2",
+				},
+			],
+		],
+	};
+
+	bot.telegram.sendMessage(ctx.chat.id, "Choose an option:", {
+		reply_markup: inlineKeyboard,
+	});
 });
+
+bot.action("button1", (ctx) => {
+	ctx.reply("You pressed button 1");
+});
+
+bot.action("button2", (ctx) => {
+	ctx.reply("You pressed button 2");
+});
+bot.telegram.setWebhook(`${process.env.WEBHOOK_URL}${route}`);
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.use(bot.webhookCallback(webhookPath));
-bot.telegram.setWebhook(`${process.env.DEPLOY_URL}/${webhookPath}`);
+app.use(bot.webhookCallback(route));
+app.use(express.static("public"));
+app.use(express.json());
+
+// bot.launch();
 
 app.listen(port, () => console.log(`Listening on ${port}`));
 
