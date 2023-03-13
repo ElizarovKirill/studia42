d3bfe57e4205dff71f077cf62ae13a233b8c849f
yshnvv <126018643+yshnvv@users.noreply.github.com>
2023-03-13 15:57:25 +0300

    shonibud

--- api/webhook.js
+++ api/webhook.js
@@ -60,18 +60,20 @@ bot.command("start", async (ctx) => {
 		inline_keyboard: [
 			[
 				{
-					text: "Find Rumor",
+					text: "Найти слухи",
 					callback_data: "find_rumor",
 				},
 				{
-					text: "Add rumor",
+					text: "Пустить слух",
 					callback_data: "add_rumor",
 				},
 			],
 		],
 	};
-
-	bot.telegram.sendMessage(ctx.chat.id, "Choose an option:", {
+const welcomeMessage = `Привет, я Сплетник - бот для создания, поиска и распространения слухов (да-да, грязных - в том числе!).\nНиже две кнопки:\n\n1. «Найти слух»\nНажми, введи имя, фамилию и город того человека, про которого хочешь узнать слухи. Если там пусто - что же, либо этот человек святой, либо дико скучный.\n
+2. «Пустить слух»\nЯ знаю, тебе есть что рассказать!\nНажми, введи имя, фамилию, возраст и город того, о ком ты хочешь написать анонимные сплетни. Пусть все знают!\n
+Не забудь скинуть меня друзьям!\n@sspletnik_bot`;
+	bot.telegram.sendMessage(ctx.chat.id, welcomeMessage, {
 		reply_markup: inlineKeyboard,
 	});
 });
@@ -80,14 +82,14 @@ const findRumorFlow = new WizardScene(
 	"findRumorFlow",
 	async (ctx) => {
 		ctx.scene.state.name = ctx.message.text;
-		await ctx.reply("Enter persons surname:", exitKeyboard);
+		await ctx.reply("Введите фамилию:", exitKeyboard);
 
 		return ctx.wizard.next();
 	},
 	
 	async (ctx) => {
 		ctx.scene.state.surname = ctx.message.text;
-		await ctx.reply("Enter persons city:", exitKeyboard);
+		await ctx.reply("Введите город:", exitKeyboard);
 
 		return ctx.wizard.next();
 	},
@@ -117,7 +119,7 @@ const findRumorFlow = new WizardScene(
 		if (rumorsText.length) {
 			const rumors = getChunks(rumorsText, RUMORS_IN_MESSAGE).map((chunk) =>
 				chunk
-					.map((rumor) => `Многие говорят: ${name} ${surname} ${rumor}`)
+					.map((rumor) => `Многие говорят: ${rumor}`)
 					.join("\n\n")
 			);
 
@@ -129,7 +131,7 @@ const findRumorFlow = new WizardScene(
 				});
 		} else {
 			await ctx.reply(
-				`There are no rumors about ${name} ${surname}.`,
+				`Судя по всему, никто ничего не написал про ${name} ${surname}. Будьте первым!`,
 				startKeyboard
 			);
 		}
@@ -137,31 +139,31 @@ const findRumorFlow = new WizardScene(
 		return ctx.scene.leave();
 	}
 );
-findRumorFlow.enter((ctx) => ctx.reply("Enter persons name:", exitKeyboard));
+findRumorFlow.enter((ctx) => ctx.reply("Введите имя (желательно - полное):", exitKeyboard));
 
 const addRumorFlow = new WizardScene(
 	"addRumorFlow",
 	async (ctx) => {
 		ctx.scene.state.name = ctx.message.text;
-		await ctx.reply("Enter persons surname:", exitKeyboard);
+		await ctx.reply("Введите фамилию:", exitKeyboard);
 
 		return ctx.wizard.next();
 	},
 	async (ctx) => {
 		ctx.scene.state.surname = ctx.message.text;
-		await ctx.reply("Enter persons age:", exitKeyboard);
+		await ctx.reply("Введите возраст:", exitKeyboard);
 
 		return ctx.wizard.next();
 	},
 	async (ctx) => {
 		ctx.scene.state.age = Number(ctx.message.text);
-		await ctx.reply("Enter persons city:", exitKeyboard);
+		await ctx.reply("Введите город:", exitKeyboard);
 
 		return ctx.wizard.next();
 	},
 	async (ctx) => {
 		ctx.scene.state.city = ctx.message.text;
-		await ctx.reply("Enter rumor about person:", exitKeyboard);
+		await ctx.reply("Напишите, что вы знаете про этого человека?:", exitKeyboard);
 
 		return ctx.wizard.next();
 	},
@@ -187,16 +189,16 @@ const addRumorFlow = new WizardScene(
 
 		const { name, surname } = ctx.scene.state;
 
-		await ctx.reply(`Rumor about ${name} ${surname} created!`, startKeyboard);
+		await ctx.reply(`Слух про ${name} ${surname} добавлен!`, startKeyboard);
 
 		return ctx.scene.leave();
 	}
 );
-addRumorFlow.enter((ctx) => ctx.reply("Enter persons name:", exitKeyboard));
+addRumorFlow.enter((ctx) => ctx.reply("Введите имя (желательно - полное):", exitKeyboard));
 
 const stage = new Stage([findRumorFlow, addRumorFlow]);
 stage.hears("exit", (ctx) => {
-	ctx.reply(`Start again?`, startKeyboard);
+	ctx.reply(`Начать заново?`, startKeyboard);
 	ctx.scene.leave();
 });
 
